56ef547b9f2145434d4adff2379b951bd9628942
anisimovv <kirill.anisimovv@gmail.com>
2022-06-05 20:01:57 +0300

    create shower-element mutation

--- src/shower-elements/shower-elements.resolver.ts
+++ src/shower-elements/shower-elements.resolver.ts
@@ -1,15 +1,29 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ResolveField,
+} from '@nestjs/graphql';
 import { ShowerElementsService } from './shower-elements.service';
 import { ShowerElement } from './entities/shower-element.entity';
 import { CreateShowerElementInput } from './dto/create-shower-element.input';
 import { UpdateShowerElementInput } from './dto/update-shower-element.input';
+import { ShowersService } from 'src/showers/showers.service';
 
 @Resolver(() => ShowerElement)
 export class ShowerElementsResolver {
-  constructor(private readonly showerElementsService: ShowerElementsService) {}
+  constructor(
+    private readonly showerElementsService: ShowerElementsService,
+    private readonly showersService: ShowersService,
+  ) {}
 
   @Mutation(() => ShowerElement)
-  createShowerElement(@Args('createShowerElementInput') createShowerElementInput: CreateShowerElementInput) {
+  createShowerElement(
+    @Args('createShowerElementInput')
+    createShowerElementInput: CreateShowerElementInput,
+  ) {
     return this.showerElementsService.create(createShowerElementInput);
   }
 
@@ -24,12 +38,26 @@ export class ShowerElementsResolver {
   }
 
   @Mutation(() => ShowerElement)
-  updateShowerElement(@Args('updateShowerElementInput') updateShowerElementInput: UpdateShowerElementInput) {
-    return this.showerElementsService.update(updateShowerElementInput.id, updateShowerElementInput);
+  updateShowerElement(
+    @Args('updateShowerElementInput')
+    updateShowerElementInput: UpdateShowerElementInput,
+  ) {
+    return this.showerElementsService.update(
+      updateShowerElementInput.id,
+      updateShowerElementInput,
+    );
   }
 
   @Mutation(() => ShowerElement)
   removeShowerElement(@Args('id', { type: () => Int }) id: number) {
     return this.showerElementsService.remove(id);
+  }
+
+  @ResolveField()
+  // TODO: resolve type issues
+  shower(showerElement) {
+    console.log(showerElement.showerId);
+    const { showerId } = showerElement;
+    return this.showersService.findOne(showerId);
   }
 }
